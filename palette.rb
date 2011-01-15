66f829866cebacb64e57491d2de1ec5acf2cc3ed
soycamo <cameron@soycow.org>
2011-01-14 19:02:49 -0800

    BROKEN, doing cleanup

--- palette.rb
+++ palette.rb
@@ -5,59 +5,77 @@
 # cameron [at] soycow <dot> org
 
 module Palette
-	class Aco
 
+	class Palette
 		attr_accessor :colors, :named_colors
 
 		def initialize(file)
 			@f = File.new(file)
 		end
+	end
 
+	class Aco < Palette
 		def colors
 			get_palette
 		end
 
-		def named_colors
-			get_palette(true)
+		private
+=begin
+		def set_colorspace
+			colorspace = { '00' => :RGB, '01' => :HSB, '02' => :CMYK, '07' => :Lab, '08' => :Grey, '09' => :WCMYK }
+			unpacker = { :RGB => 'xxH2xxH2H2', :HSB => '', :CMYK => '', :Lab => '', :Grey => '', :WCMYK => '', }
+			pos = @f.pos
+			@f.read(2).unpack('H2')[0]
+			
+			@f.pos = pos
 		end
-
-private
-		def get_palette(named=false)
+=end
+		def get_palette
 			# thanks to http://www.nomodes.com/aco.html
-			# First two bytes are "0001" (section one).
-			# Second two bytes are the number of colors.
-			# Then a 10 byte code of the following (RGB only):
-			#	0000 rrrr gggg bbbb 0000
 			@colors = []
+			#RGBColorEntry = Struct.new(:name, :red, :green, :blue)
+			#CMYKColorEntry = Struct.new(:name, :cyan, :magenta, :yellow, :black)
 			version = (@f.read(2)).unpack('c*').to_s
 			num = (@f.read(2)).unpack('H*')[0].hex
+			#set_colorspace # We're assuming that the file has only one color type.
+			p version
+			if version == "02" || (version == "01" && has_names(num))
+				# check to see if it can get names instead.
+				p "you can get the names!!"
+			else
+				# just return the colors.
+				p "you can only get colors, sorry"
+			end
+
+=begin
 			case version
 				when "01"
-				@f.seek(num * 10 + 8); pos = @f.pos
-				if ( pos < File.size(@f)) && named 
+					@f.seek 4
+					num.times do	
+						col = @f.read(10).unpack('H2H2H2H2H2H2H2H2H2H2')
+						p col
+						@colors << col.to_s
+					end	
+				when "02"
 					@f.seek 4
 					@f.seek(num * 10 + 8)
 					num.times do
-						@colors << @f.read(10).unpack('xxH2xxH2H2').to_s
+						@colors << @f.read(10).unpack('H2H2H2H2H2H2H2H2H2H2').to_s
 						len = (@f.read(4)).unpack('xxxU')[0]
 						@colors << @f.read(len*2).unpack('xa' * len).to_s.rstrip
 					end
 					@colors = Hash[*@colors.flatten]
-				else
-					@f.seek 4
-					num.times do 
-						@colors << @f.read(10).unpack('xxH2xxH2H2').to_s
-					end	
-				end
-				@f.rewind
-			when "02"
-				p "version 3"
 			else
 				p "unknown version"
 			end
+			@f.rewind
+=end
 			return @colors
 		end
 
+		def has_names(entries)
+			return true if (@f.seek(entries * 10 + 8); @f.pos < File.size(@f))
+		end
 	end
 
 	class Gpl 
@@ -78,7 +96,7 @@ end
 
 if !ARGV[0].nil? && File.file?(ARGV[0])
 	aco = Palette::Aco.new(ARGV[0]).colors
-	p aco
+	p aco[0..10]
 	p aco.length
 else
 	puts "Usage: palette.rb filename.aco"
